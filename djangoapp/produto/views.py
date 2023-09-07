ae10c49b864c87497d6997c7772a895b820c88fb
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-09-07 13:58:06 +0100

    Lista e detalhes de produtos

--- djangoapp/produto/views.py
+++ djangoapp/produto/views.py
@@ -1,5 +1,6 @@
 from django.views import View
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from . import models
 
@@ -7,11 +8,16 @@ from . import models
 class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
+    ordering = ['-id']
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("DetalheProduto")
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
